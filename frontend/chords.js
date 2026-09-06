b9c8e5a995f92c8594d128b0f3ac690a2a8507d4
Computer002 <Computer002@DESKTOP-3N4RTD4.>
2026-09-06 16:03:12 +0800

    update

--- frontend/chords.js
+++ frontend/chords.js
@@ -5,9 +5,10 @@ import ChordSheetJS from "https://cdn.jsdelivr.net/npm/chordsheetjs@16.2.2/+esm"
 const chordSheetText = `
 {title: My Song}
 {key: C}
-{chord_style: number}
+{chord_style: symbol}
 This is [C]the first line
-This is [G]the second line
+This is [G]the second line~
+Third line with a [F]chord
 `.substring(1);
 const song = new ChordSheetJS.ChordProParser().parse(chordSheetText);
 const formatter = new ChordSheetJS.HtmlTableFormatter();
